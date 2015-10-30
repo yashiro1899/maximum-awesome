cb8112c3abeb40a4af26ba896dc3753ad7b5c6f0
Ice White <yashiro1899@gmail.com>
2015-10-30 19:07:33 +0800

    1446203253

---
+++ bin/manchester.js
@@ -0,0 +1,81 @@
+/*property
+    HOME, Server, connect, createReadStream, createServer, css, end, env, error, extname, filepath, headers, hostname, html, indexOf, join, js, json, keys, length, listen, on, parse, pipe, port, size, stack, statSync, url, write, writeHead
+*/
+
+'use strict';
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const net = require('net');
+const url = require('url');
+
+const MIME = {
+    css: 'text/css',
+    html: 'text/html',
+    js: 'application/javascript',
+    json: 'application/json'
+};
+
+var server = http.createServer(function (req, res) {
+    var configs = require(path.join(process.env.HOME, '.manchester.json'));
+    var prefixes = Object.keys(configs);
+
+    var i = 0,
+        l = prefixes.length;
+    var prefix, conf, stat, headers;
+    while (i < l) {
+        prefix = prefixes[i];
+        if (req.url.indexOf(prefix) > -1) {
+            conf = configs[prefix];
+            stat = fs.statSync(conf.filepath);
+
+            headers = conf.headers || {};
+            headers['Content-Length'] = stat.size;
+            headers.Server = 'Manchester';
+            if (headers['Content-Type'] === undefined) {
+                headers['Content-Type'] = MIME[path.extname(conf.filepath)];
+            }
+
+            res.writeHead(200, headers);
+            fs.createReadStream(conf.filepath)
+                .pipe(res);
+
+            return null;
+        }
+
+        i += 1;
+    }
+
+    res.end('kk\n');
+});
+
+server.listen(8122);
+server.on('clientError', function (err) {
+    console.error('\x1b[31m[client]\x1b[0m\n%s', err.stack);
+});
+
+// directly forward https request
+server.on('connect', function (req, socket, head) {
+    var parsedURL = url.parse('https://' + req.url);
+
+    var proxySocket = net.connect(parsedURL.port, parsedURL.hostname, function () {
+        socket.write('HTTP/1.1 200 Connection Established\r\n\r\n');
+
+        this.write(head);
+        socket.pipe(this);
+    });
+
+    proxySocket.pipe(socket);
+    proxySocket.on('error', function (err) {
+        console.error('\x1b[31m[https]\x1b[0m\n%s', err.stack);
+    });
+});
+
+// directly forward websocket
+server.on('upgrade', function (ignore, socket) {
+    socket.write('HTTP/1.1 101 Web Socket Protocol Handshake\r\n' +
+        'Upgrade: WebSocket\r\n' +
+        'Connection: Upgrade\r\n\r\n');
+
+    socket.pipe(socket);
+});
